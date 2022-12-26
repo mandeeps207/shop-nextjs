f13336e4819ab5d3d7b92ad4af59b79e1a0ef6f0
mandeeps207 <mandeeps207@outlook.com>
2022-12-27 02:01:26 +0530

    items list added on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Layout from '../components/Layout';
+import Productitem from '../components/Productitem';
+import data from '../utils/product.json';
 
 export default function Home() {
-  return <Layout title="XM42 | Homepage">Homepage</Layout>;
+  return (
+    <Layout title="XM42 | Homepage">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <Productitem product={product} key={product.id}></Productitem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
